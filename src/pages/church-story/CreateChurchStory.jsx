e24a95dcdd62fcfb577df266664f6bb5317bc578
Haimanot Beka Mekonnen <haimanotbeka@gmail.com>
2026-08-04 09:01:03 +0300

    Fix import statement for CreateChurchStory.css

--- src/pages/church-story/CreateChurchStory.jsx
+++ src/pages/church-story/CreateChurchStory.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import API from "../../api/api";
-import "./createChurchStory.css";
+import "./CreateChurchStory.css";
 
 const CreateChurchStory = () => {
   const { t } = useTranslation();
